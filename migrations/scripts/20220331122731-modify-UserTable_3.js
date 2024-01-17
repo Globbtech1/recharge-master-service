b4b1a8d0605ab781c84b32bd9d2a4b0310cdf80f
jinad david <hashdavies@gmail.com>
2024-01-17 09:15:40 +0000

    remove role  migration

--- migrations/scripts/20220331122731-modify-UserTable_3.js
+++ migrations/scripts/20220331122731-modify-UserTable_3.js
@@ -1,38 +1,38 @@
-"use strict";
-module.exports = {
-  up: async (queryInterface, DataTypes) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    return Promise.all([
-      queryInterface.addColumn("users", "role", {
-        type: DataTypes.STRING,
-        allowNull: false,
-        comment: "Access role of the user",
-        defaultValue: "customer",
-      }),
+// "use strict";
+// module.exports = {
+//   up: async (queryInterface, DataTypes) => {
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//     return Promise.all([
+//       queryInterface.addColumn("users", "role", {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         comment: "Access role of the user",
+//         defaultValue: "customer",
+//       }),
 
-      queryInterface.addColumn("users", "isActive", {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true,
-      }),
-    ]);
-  },
+//       queryInterface.addColumn("users", "isActive", {
+//         type: DataTypes.BOOLEAN,
+//         allowNull: false,
+//         defaultValue: true,
+//       }),
+//     ]);
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('transactions_history');
-     */
-    return Promise.all([
-      queryInterface.removeColumn("users", "role"),
-      queryInterface.removeColumn("users", "isActive"),
-    ]);
-  },
-};
+//   down: async (queryInterface, Sequelize) => {
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('transactions_history');
+//      */
+//     return Promise.all([
+//       queryInterface.removeColumn("users", "role"),
+//       queryInterface.removeColumn("users", "isActive"),
+//     ]);
+//   },
+// };
