78121c8f2f1df3163f95351fd0219b56732d619b
jinad david <hashdavies@gmail.com>
2024-01-17 09:57:37 +0000

    update usertable 3 migration and package json

--- migrations/scripts/20220331122731-modify-UserTable_3.js
+++ migrations/scripts/20220331122731-modify-UserTable_3.js
@@ -1,38 +1,43 @@
-// "use strict";
-// module.exports = {
-//   up: async (queryInterface, DataTypes) => {
-//     /**
-//      * Add altering commands here.
-//      *
-//      * Example:
-//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-//      */
-//     return Promise.all([
-//       queryInterface.addColumn("users", "role", {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         comment: "Access role of the user",
-//         defaultValue: "customer",
-//       }),
+"use strict";
 
-//       queryInterface.addColumn("users", "isActive", {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//         defaultValue: true,
-//       }),
-//     ]);
-//   },
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    // Check if the column 'role' doesn't exist before adding it
+    const hasRoleColumn = await queryInterface.describeTable("users").then(tableDefinition => !!tableDefinition.role);
 
-//   down: async (queryInterface, Sequelize) => {
-//     /**
-//      * Add reverting commands here.
-//      *
-//      * Example:
-//      * await queryInterface.dropTable('transactions_history');
-//      */
-//     return Promise.all([
-//       queryInterface.removeColumn("users", "role"),
-//       queryInterface.removeColumn("users", "isActive"),
-//     ]);
-//   },
-// };
+    if (!hasRoleColumn) {
+      await queryInterface.addColumn("users", "role", {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: "Access role of the user",
+        defaultValue: "customer",
+      });
+    }
+
+    // Check if the column 'isActive' doesn't exist before adding it
+    const hasIsActiveColumn = await queryInterface.describeTable("users").then(tableDefinition => !!tableDefinition.isActive);
+
+    if (!hasIsActiveColumn) {
+      await queryInterface.addColumn("users", "isActive", {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      });
+    }
+
+    return Promise.resolve(); // Or any other result you want to return
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('transactions_history');
+     */
+    return Promise.all([
+      queryInterface.removeColumn("users", "role"),
+      queryInterface.removeColumn("users", "isActive"),
+    ]);
+  },
+};
